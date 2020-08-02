6e481ab6ad88d05aedb24c8a4a72e76430c66d86
EddyMelly <57482261+EddyMelly@users.noreply.github.com>
2020-08-02 11:28:16 +0100

    powerup changes

--- powerUp.js
+++ powerUp.js
@@ -1,32 +1,31 @@
-import {powerUpCollision} from './collisionDetection.js';
-export default class PowerUp{
-    constructor(game, player){
-        this.game = game;
-        this.player = player;
-        if(this.player.position.x < this.game.gameWidth/2){
-            this.position = {x: 700, y: this.player.position.y};
-        }else{
-            this.position = {x: 50, y: this.player.position.y};
-        }
-        this.image = document.getElementById('ceremorTense');
-        this.active = true;
-        this.appearTime = 10;
-        this.appearTimeStart = 0;
+import { powerUpCollision } from './collisionDetection.js';
+export default class PowerUp {
+  constructor(game, player) {
+    this.game = game;
+    this.player = player;
+    if (this.player.position.x < this.game.gameWidth / 2) {
+      this.position = { x: 600, y: this.player.position.y };
+    } else {
+      this.position = { x: 150, y: this.player.position.y };
     }
+    this.image = document.getElementById('ceremorTense');
+    this.active = true;
+    this.appearTime = 10;
+    this.appearTimeStart = 0;
+  }
 
-    update(deltaTime){
-        this.appearTimeStart += deltaTime /1000;
-        if(this.appearTimeStart > 14){
-            this.active = false;
-        }
-        if(powerUpCollision(this.player, this)){
-            this.player.automaticOn();
-            this.active = false;
-        }
-
+  update(deltaTime) {
+    this.appearTimeStart += deltaTime / 1000;
+    if (this.appearTimeStart > 14) {
+      this.active = false;
     }
+    if (powerUpCollision(this.player, this)) {
+      this.player.automaticOn();
+      this.active = false;
+    }
+  }
 
-    draw(ctx) {
-        ctx.drawImage(this.image, this.position.x, this.position.y , 50, 50);
-      }
+  draw(ctx) {
+    ctx.drawImage(this.image, this.position.x, this.position.y, 50, 50);
+  }
 }
